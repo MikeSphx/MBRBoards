f6d00db59f440361c57f37209506beadb0ac7171
MikeSphx <kim.michae@husky.neu.edu>
2017-02-25 05:23:06 -0500

    Worked on Express server and started front end mockup

---
+++ demo/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function() {
+	console.log('listening on 3000');
+})
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/test', function(req, res){
+	res.sendFile(__dirname + '/test.html');
+})
+
+app.post('/quotes', function(req, res) {
+	console.log(req.body);
+
+})
