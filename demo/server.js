7071aca381ce04f8fc6f05023c54d2b5d6dab3de
MikeSphx <kim.michae@husky.neu.edu>
2017-02-26 16:05:04 -0500

    Incoporating MongoDB into the demo

--- demo/server.js
+++ demo/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
-	console.log('listening on 3000');
+var db;
+
+dbLink = 'mongodb://mikesphx-admin:pillsNPotionsLiftMeUp@ds161049.mlab.com:61049/mbrboards-demo-db'
+
+MongoClient.connect(dbLink, function(err, database) {
+	// start the server
+	if (err) return console.log(err);
+	db = database;
+	app.listen(3000, function() {
+		console.log('listening on 3000');
+	})
 })
 
 app.get('/', function(req, res) {
@@ -18,6 +28,10 @@ app.get('/test', function(req, res){
 })
 
 app.post('/quotes', function(req, res) {
-	console.log(req.body);
+	db.collection('quotes').save(req.body, function(err, result) {
+		if (err) return console.log(err);
 
+		console.log('saved to database');
+		res.redirect('/test')
+	})
 })
